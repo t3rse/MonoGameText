f72a0e9f21d3aa90ea9d55518cd58a88d6a966ee
t3rse <david.seruyange@gmail.com>
2018-10-17 13:46:06 -0700

    Image loading functionality

---
+++ TextureImages/Game1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Net.Http;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextureImages
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        System.Net.Http.HttpClient client;
+        Texture2D imageTexture;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            client = new System.Net.Http.HttpClient();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override async void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            // TODO: use this.Content to load your game content here
+            var response = await client
+                                     .GetAsync("https://images.hanselminutes.com/images/646.jpg", HttpCompletionOption.ResponseContentRead)
+                                     .ConfigureAwait(false);
+
+            var result = await response.Content.ReadAsByteArrayAsync();
+
+            var memStream = new MemoryStream(result);
+            imageTexture = Texture2D.FromStream(GraphicsDevice, memStream);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+ 
+            if(imageTexture != null) { 
+                spriteBatch.Begin();
+                spriteBatch.Draw(imageTexture, new Vector2(10, 10), Color.AliceBlue);
+                spriteBatch.End();
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
